8afbc21d1161b98112f7ddc990eef77e53f0d485
Sean Schnell <seansps@gmail.com>
2024-10-17 19:40:21 -0400

    Add roll handler for skills

---
+++ rollhandlers/ability.js
@@ -0,0 +1,24 @@
+const name = data?.roll?.metadata?.rollName;
+const tooltip = data?.roll?.metadata?.tooltip;
+
+let message = '';
+
+let dc = 0;
+dc = parseInt(data?.roll?.metadata?.dc || '0', 10);
+if (isNaN(dc)) {
+  dc = 0;
+}
+if (dc > 0) {
+  const total = data?.roll?.total || 0;
+  if (total >= dc) {
+    message = `**[center][color=green]SUCCESS[/color] [gm]vs DC ${dc}[/gm][/center]**`
+  }
+  else {
+    message = `**[center][color=red]FAILURE[/color] [gm]vs DC ${dc}[/gm][/center]**`
+  }
+}
+
+api.sendMessage(message, data.roll, [], [{
+  name: name || "Ability",
+  tooltip: tooltip || `${name || ""} Ability Roll`
+}]);
