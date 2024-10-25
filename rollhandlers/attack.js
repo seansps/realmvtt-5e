4e0475daa4df00f4ad839ba164d4bc46ecfaa626
Sean Schnell <seansps@gmail.com>
2024-10-25 12:30:16 -0400

    Fix output and crit miss/hit

--- rollhandlers/attack.js
+++ rollhandlers/attack.js
@@ -14,8 +14,11 @@ const damageModifiers = data?.roll?.metadata?.damageModifiers || [];
 const icon = data?.roll?.metadata?.icon;
 let damage = data?.roll?.metadata?.damage;
 
-const isCritical = data?.roll?.total - data?.roll?.modifier >= 20; // Should never be > 20, but just in case
-const isMiss = data?.roll?.total - data?.roll?.modifier <= 1; // Should never be < 1, but just in case
+// If the d20 was a 20, it's a critical hit
+const d20 = (data?.roll?.dice || []).find(d => d.type === 20 && d.reason !== 'dropped');
+const isCritical = d20 && d20.value === 20;
+// If the d20 was a 1, it's a miss
+const isMiss = d20 && d20.value === 1;
 
 let message = '';
 
@@ -24,28 +27,27 @@ dc = parseInt(data?.roll?.metadata?.dc || '0', 10);
 if (isNaN(dc)) {
   dc = 0;
 }
-if (dc > 0) {
-  const total = data?.roll?.total || 0;
-  if (isCritical) {
-    message = `[center]${icon ? `:${icon}:` : ''} ${attack} :IconTargetArrow: ${targetName}[/center]\n\n**[center][color=green]CRITICAL HIT[/color] [gm](vs AC ${dc})[/gm][/center]**
-  `
-    // If damage was defined, we need to double the dice 
-    if (damage) {
-      damage = doubleDamageDice(damage);
-    }
-  }
-  else if (isMiss) {
-    message = `[center]${icon ? `:${icon}:` : ''} ${attack} :IconTargetArrow: ${targetName}[/center]\n\n**[center][color=red]AUTOMATIC MISS[/color] [gm](vs AC ${dc})[/gm][/center]**`
-  }
-  else if (total >= dc) {
-    message = `[center]${icon ? `:${icon}:` : ''} ${attack} :IconTargetArrow: ${targetName}[/center]\n\n**[center][color=green]HIT[/color] [gm](vs AC ${dc})[/gm][/center]**`
-  }
-  else {
-    message = `[center]${icon ? `:${icon}:` : ''} ${attack} :IconTargetArrow: ${targetName}[/center]\n\n**[center][color=red]MISS[/color] [gm](vs AC ${dc})[/gm][/center]**`
+
+const total = data?.roll?.total || 0;
+if (isCritical) {
+  message = `[center]${icon ? `:${icon}:` : ''} ${attack} ${targetName ? ` :IconTargetArrow: ${targetName}` : ''}[/center]\n\n**[center][color=green]CRITICAL HIT[/color] [gm]${dc > 0 ? `(vs AC ${dc})` : ''}[/gm][/center]**
+`
+  // If damage was defined, we need to double the dice 
+  if (damage) {
+    damage = doubleDamageDice(damage);
   }
 }
+else if (isMiss) {
+  message = `[center]${icon ? `:${icon}:` : ''} ${attack} ${targetName ? ` :IconTargetArrow: ${targetName}` : ''}[/center]\n\n**[center][color=red]AUTOMATIC MISS[/color] [gm]${dc > 0 ? `(vs AC ${dc})` : ''}[/gm][/center]**`
+}
+else if (total >= dc && dc > 0) {
+  message = `[center]${icon ? `:${icon}:` : ''} ${attack} ${targetName ? ` :IconTargetArrow: ${targetName}` : ''}[/center]\n\n**[center][color=green]HIT[/color] [gm](vs AC ${dc})[/gm][/center]**`
+}
+else if (dc > 0) {
+  message = `[center]${icon ? `:${icon}:` : ''} ${attack} ${targetName ? ` :IconTargetArrow: ${targetName}` : ''}[/center]\n\n**[center][color=red]MISS[/color] [gm](vs AC ${dc})[/gm][/center]**`
+}
 else {
-  message = `[center]${icon ? `:${icon}:` : ''} ${attack} :IconTargetArrow: ${targetName}[/center]`
+  message = `[center]${icon ? `:${icon}:` : ''} ${attack} ${targetName ? ` :IconTargetArrow: ${targetName}` : ''}[/center]`
 }
 
 const tags = [{
