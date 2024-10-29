9228c2e0fe3a9ec721941fd5ad301bdb718e9ee8
Sean Schnell <seansps@gmail.com>
2024-10-29 09:59:27 -0400

    Add working skills to NPCs. Wire up initiative

--- rollhandlers/initiative.js
+++ rollhandlers/initiative.js
@@ -8,13 +8,7 @@ if (data.roll?.metadata?.group && data.roll?.metadata?.group.length > 0) {
 	}])
 }
 else {
-	if (data.roll?.metadata?.token) {
-		api.setValueOnToken(data.roll?.metadata?.token, 'data.initiative', data.roll.total);
-	}
-	else if (data.roll?.metadata?.isCharacter) {
-		api.setValue('data.initiative', data.roll.total);
-	}
-
+	api.setValue('data.initiative', data.roll.total);
 	api.sendMessage("", data.roll, [], [{
 		name: "Initiative",
 		tooltip: "Initiative Roll"
