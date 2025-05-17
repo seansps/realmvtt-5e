cbd2bcdc077a3e004399c5b462fc079dc103bf25
Sean Schnell <seansps@gmail.com>
2025-05-17 15:16:57 -0400

    Implemenet regeneration effects.

--- rollhandlers/onTurnStart.js
+++ rollhandlers/onTurnStart.js
@@ -42,3 +42,64 @@ if (token?.data?.usedLegendaryActions) {
     api.setValueOnToken(token, "data.usedLegendaryActions", 0);
   }
 }
+
+// Check for Regeneration effects
+const modifiers = getEffectsAndModifiersForToken(token, ["regeneration"], "");
+
+// This gets applied to the token's HP via a macro
+let healing = 0;
+let effectsNames = "";
+modifiers.forEach((modifier) => {
+  if (typeof modifier.value === "number" && modifier.value > 0) {
+    healing += modifier.value;
+    if (effectsNames !== "") {
+      effectsNames += ", ";
+    }
+    effectsNames += modifier.name;
+  }
+});
+
+if (healing > 0 && token.data?.curhp < token.data?.hitpoints) {
+  const healingMacro = `\`\`\`Apply_Healing
+const healing = ${healing};
+
+let targets = api.getSelectedOrDroppedToken();
+
+// If record is not null, check if we're the GM or owner and use it
+if (record) {
+  if (isGM || record?.record?.ownerId === userId) {
+    targets = [record];
+  }
+}
+
+// If we're a player and we did not drop on a record, get our owned tokens
+if (!isGM && targets.length === 0) {
+    targets = api.getSelectedOwnedTokens().map(target => target.token);
+}
+
+targets.forEach(target => {
+  // Apply healing
+  if (target && target.data) {
+    var curhp = target.data?.curhp || 0;
+    curhp += healing;
+    if (curhp > target.data?.hitpoints) { curhp = target.data?.hitpoints; }
+    const oldHp = (target.data?.curhp || 0);
+    api.setValueOnToken(target, "data.curhp", curhp);
+    const unIdentified = target.identified === false;
+    const targetName = !unIdentified ? target.name || target.record.name : target.unidentifiedName || target.record.unidentifiedName;
+
+    const macro = \`\\\`\\\`\\\`Undo\\n if (isGM) { api.setValueOnTokenById('\$\{target._id\}', '\$\{target.recordType\}', 'data.curhp', '\$\{oldHp\}'); api.editMessage(null, '~\$\{targetName\} healed for \$\{healing\} HP.~'); } else { api.showNotification('Only the GM can undo healing.', 'yellow', 'Notice'); } \\n\\\`\\\`\\\`\`;
+    
+    api.sendMessage(\`\$\{targetName\} healed for \$\{healing\} HP.\\n\$\{macro\}\`, undefined, undefined, undefined, target);
+
+    // If healing > 0, float text
+    if (healing > 0) {
+      api.floatText(target, \`+\${healing}\`, "#1bc91b");
+    }
+  }
+});
+\`\`\``;
+
+  const message = `Regenerates ${healing} HP, unless deactivated.\n\n${healingMacro}`;
+  api.sendMessage(message, undefined, [], [], token);
+}
