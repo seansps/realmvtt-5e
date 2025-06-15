73278aded3fc9327377242363461b20750d3e4ae
Sean Schnell <seansps@gmail.com>
2025-06-15 19:36:44 -0400

    Fix recharge for other action groups

--- rollhandlers/recharge.js
+++ rollhandlers/recharge.js
@@ -13,17 +13,30 @@ const token = api.getRecord("tokens", tokenId, (token) => {
     api.removeEffectById(effectId, token);
     message = `${actionName} recharges.`;
     // Find the action in their list with this name
-    const actions = token?.data?.actions || [];
-    const actionIndex = actions.findIndex((action) =>
-      (action?.name || "").toLowerCase().startsWith(actionName.toLowerCase())
-    );
+    let group = "actions";
+    let groups = ["actions", "bonusActions", "reactions", "legendaryActions"];
+    let actionIndex = -1;
+    for (const curGroup of groups) {
+      const actions = token?.data?.[curGroup] || [];
+      if (actionIndex === -1) {
+        actionIndex = actions.findIndex((action) =>
+          (action?.name || "")
+            .toLowerCase()
+            .startsWith(actionName.toLowerCase())
+        );
+      }
+      if (actionIndex !== -1) {
+        group = curGroup;
+        break;
+      }
+    }
     if (actionIndex !== -1) {
-      const action = actions[actionIndex];
+      const action = token?.data?.[group]?.[actionIndex];
       const uses = action.data.uses || 0;
       if (uses > 0) {
         api.setValueOnToken(
           token,
-          `data.actions.${actionIndex}.data.uses`,
+          `data.${group}.${actionIndex}.data.uses`,
           uses - 1
         );
       }
